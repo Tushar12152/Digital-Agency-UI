5d6430fefa76a2f537bc10843dcdbbcf53039751
Tushar12152 <tusharimran470@gmail.com>
2024-01-30 20:51:08 +0600

    Banner added with responsive

--- src/Routs/Routs.jsx
+++ src/Routs/Routs.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layouts/Layout";
+import Home from "../Components/Home";
 
 const Routs = createBrowserRouter([
     {
         path:'/',
-        element:<Layout/>
+        element:<Layout/>,
+        children:[
+            {
+                path:'/',
+                element:<Home/>
+            }
+        ]
     }
 ])
 

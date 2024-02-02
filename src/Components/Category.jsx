018c1d3ab35e6824bf8dcab781823f1fd5de104a
Tushar12152 <tusharimran470@gmail.com>
2024-02-02 13:07:33 +0600

    cartegoris section is added with responsive

---
+++ src/Components/Category.jsx
@@ -0,0 +1,53 @@
+import Container from "../Container/Container";
+
+
+const Category = () => {
+
+    const CategoriesData=[
+        {
+            id:1,
+            logo:'https://i.ibb.co/4twGkHW/Services-UIUX-Jim-Fahad-Digital-1.png',
+            title:'UI/UX Design',
+            description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+        },
+        {
+            id:2,
+            logo:'https://i.ibb.co/mN3Py42/Services-Web-Design-Jim-Fahad-Digital-1.png',
+            title:'Web Design',
+            description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+        },
+        {
+            id:3,
+            logo:'https://i.ibb.co/PrmG54X/Services-Branding-Jim-Fahad-Digital-1.png',
+            title:'Branding',
+            description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+        },
+    ]
+
+
+    return (
+        <div>
+             <Container>
+                    <div className="bg-gray-400 rounded-2xl mt-20">
+                          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-16">
+                               {
+                                  CategoriesData.map(data=><div key={data.id}>
+                                      
+                                      <div className="hover:bg-white rounded-xl m-4 p-8 w-[80%]">
+                                           <img className="w-20" src={data.logo} alt="" />
+                                           <h1 className="font-bold text-2xl py-5 text-black">{data?.title}</h1>
+                                           <p className="pr-12">{data?.description}</p>
+
+                                           <a className="mt-10 font-bold" href="#">Learn more→</a>
+                                      </div>
+                                       
+                                  </div>)
+                               }
+                          </div>
+                    </div>
+             </Container>
+        </div>
+    );
+};
+
+export default Category;

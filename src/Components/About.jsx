9754d4c2cea9f9ddb6a90b4eb3cb3a2a3725cbfa
Tushar12152 <tusharimran470@gmail.com>
2024-02-12 22:13:48 +0600

    modified about us page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -13,34 +13,34 @@ const About = () => {
                    <div className="w-[50%]">
                         <h1 className="font-bold text-5xl ">Content Has to Be More That Just Brilliant</h1>
                         <p className="text-gray-400 py-4 ">Lorem ipsum dolor sit amet, sed nulla ante amet, elementum tincidunt arcu sed laoreet, natoque ac eget imperdiet. Ac scelerisque nibh dolores consectetuer, nulla aptent est pede. Scelerisque euismod varius mi, congue eget sed vestibulum, ornare cras sed nec.</p>
-                        <button className="px-5 rounded-2xl text-white hover:bg-yellow-400 py-3 bg-blue-500">Our Services</button>
+                        <button className="px-5 rounded-2xl text-white hover:bg-yellow-400 py-3 bg-blue-500 hover:text-black">Our Services</button>
                    </div>
 
                    <div className="w-[50%]">
                         <div className="flex gap-6">
                               <div>
-                                    <div className="bg-gray-400 p-20 text-center mt-10">
-                                        <h1 className="text-red-600 font-bold text-2xl">210+</h1>
+                                    <div className="bg-gray-400 rounded-2xl p-20 text-center mt-10">
+                                        <h1 className="text-red-600 font-bold text-5xl">210+</h1>
                                         <p className="">Satisfied Customers</p>
                                     </div>
 
-                                    <div className="bg-gray-400 p-20 mt-10 text-center">
-                                        <h1 className="text-red-600 font-bold text-2xl">210+</h1>
-                                        <p className="">Satisfied Customers</p>
+                                    <div className="bg-gray-400 rounded-2xl p-20 mt-10 text-center">
+                                        <h1 className="text-blue-600 font-bold text-5xl">110+</h1>
+                                        <p className="">Creative Employees</p>
                                     </div>
 
                               </div>
                               <div>
 
                               <div>
-                                    <div className="bg-gray-400 p-20 text-center">
-                                        <h1 className="text-red-600 font-bold text-2xl">210+</h1>
-                                        <p className="">Satisfied Customers</p>
+                                    <div className="bg-gray-400 rounded-2xl p-20 text-center">
+                                        <h1 className="text-blue-900 font-bold text-5xl">15k</h1>
+                                        <p className="">Projects Finished</p>
                                     </div>
 
-                                    <div className="bg-gray-400 p-20 mt-10 text-center">
-                                        <h1 className="text-red-600 font-bold text-2xl">210+</h1>
-                                        <p className="">Satisfied Customers</p>
+                                    <div className="bg-gray-400 rounded-2xl p-20 mt-10 text-center">
+                                        <h1 className="text-violet-800 font-bold text-5xl">12+</h1>
+                                        <p className="">International Awards</p>
                                     </div>
                               </div>
                         </div>
